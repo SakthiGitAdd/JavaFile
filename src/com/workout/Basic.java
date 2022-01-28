0e46ef6241e7358b0788731910f1268c3c260ec4
SakthiGitAdd <sakthi36ksr@gmail.com>
2022-01-29 00:07:39 +0530

    First Move

---
+++ src/com/workout/Basic.java
@@ -0,0 +1,19 @@
+package com.workout;
+
+public class Basic {
+	private void customerName() {
+		System.out.println("Customer Name is : sakthi venkatesh kumaran kaithamalai ");
+	}
+	private void customerId() {
+		System.out.println("Customer Id is : 897456");
+	}
+		
+	public static void main(String[] args) {
+		Basic a = new Basic();
+		a.customerName();
+		a.customerId();
+		}
+	
+}
+
+
